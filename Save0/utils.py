2ee2756474d4a02a7d1858e4656ec922a0b19fe7
Daniël Geerts <g.daniel.geerts@gmail.com>
2026-05-31 23:00:11 +0200

    Move code around

--- Save0/utils.py
+++ Save0/utils.py
@@ -1,16 +1,18 @@
+# VARIABLES
 list_of_hats = []
 for hat in Hats:
 	if num_unlocked(hat) == 1:
 		list_of_hats.append(hat)
+
+#METHODS
+def next_random_hat():
+	change_hat(list_of_hats[random() * len(list_of_hats) // 1])
 
 def half_world_size():
 	return (get_world_size() / 2) - 1
 
 def pumpkin_size():
 	return 3
-
-def next_random_hat():
-	change_hat(list_of_hats[random() * len(list_of_hats) // 1])
 
 def water():
 	if num_items(Items.Water) > 5:
